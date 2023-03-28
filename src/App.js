73bb8070717b4a7aaecf80b5ea49ae0472c27e06
alexb017 <24658786+alexb017@users.noreply.github.com>
2023-03-28 17:53:02 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import Navbar from './Navbar';
+import Header from './Header';
+import Feature from './Feature';
+import Work from './Work';
+import Unique from './Unique';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="main">
+      <Navbar />
+      <div className="container">
+        <Header />
+        <hr />
+        <hr />
+        <Feature />
+        <hr />
+        <Work />
+        <hr />
+        <hr />
+        <Unique />
+        <hr />
+        <hr />
+      </div>
+    </main>
+  )
 }
 
 export default App;
---
+++ src/Feature.js
@@ -0,0 +1,15 @@
+export default function Feature() {
+    return (
+        <section className="feature">
+            <div className="feature-content grid">
+                <div className="feature-left">
+                    <h2 className="feature-h2">Say hello to your new superpower</h2>
+                    <h5 className="feature-h5">With a library of exercises, plus exciting ways to challenge friends and interact with a global community, Fitr arms you with the power to fight stress, fatigue and lack of focus.</h5>
+                </div>
+                <div className="feature-right">
+                    <img className="feature-img img" width="100%" height="auto" src="https://res.cloudinary.com/drbu50m1e/image/upload/v1680000172/react-static-page/thomas-yohei-BAlBUJb-SXQ-unsplash_dfnvfb.jpg" alt="thomas-yohei-BAlBUJb-SXQ-unsplash_dfnvfb" />
+                </div>
+            </div>
+        </section>
+    )
+}
